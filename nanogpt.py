345c80aed5b8e312e5db1a7e4e9cfa4973561b06
robrosk <rob.roskowski@gmail.com>
2026-07-26 15:13:54 -0700

    separation of concerns

--- nanogpt.py
+++ nanogpt.py
@@ -1,19 +1,8 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
-# hyperparams
-batch_size = 64
-block_size = 256
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layers = 6
-dropout = 0.2
+from hyperparams import batch_size, block_size, device, dropout, eval_interval, eval_iters, learning_rate, max_iters, n_embd, n_head, n_layers
+from attention import Block
 
 torch.manual_seed(1337)
 
@@ -64,36 +53,8 @@ def estimate_loss():
     model.train()
     return out
 
-class FeedForward(nn.Module):
-    """ a simple linear layer followed by a non-linearity """
-    def __init__(self, n_embd):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
-            nn.Dropout(dropout),
-        )
-        
-    def forward(self, x):
-        return self.net(x)
-
-class MultiHeadAttention(nn.Module):
-    """ multiple heads of self-attention in parallel """
-    
-    def __init__(self, num_heads, head_size):
-        super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(n_embd, n_embd)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.dropout(self.proj(out))
-        return out
-
 # simple bigram model
-class BigramLanguageModel(nn.Module):
+class NanoGPT(nn.Module):
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
@@ -141,52 +102,8 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
 
         return idx
-    
-class Head(nn.Module):
-    """ one head of self-attention """
-    
-    def __init__(self, head_size):
-        super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-        
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        B,T,C = x.shape
-        k = self.key(x) # (B,T,C)
-        q = self.query(x) # (B,T,C)
-        
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * C ** (-0.5) # (B,T,C) @ (B,C,T) ---> (B,T,T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B,T,T)
-        wei = F.softmax(wei, dim=-1) # (B,T,T)
-        
-        # perform the weighted aggregation of the values
-        v = self.value(x)
-        out = wei @ v # (B, T, T) @ (B, T, C) ---> (B, T, C)
-        return out
 
-class Block(nn.Module):
-    """ Transformer block: communication followed by computation """
-    
-    def __init__(self, n_embd, n_head):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
-        super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embd)
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
-        
-    def forward(self, x):
-        x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
-        return x
-
-model = BigramLanguageModel()    
+model = NanoGPT()    
 m = model.to(device)
 
 # print the number of parameters in the model
